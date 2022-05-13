16bcc9ea523bceff3cf2bcab14c5be2f06c84b41
marlxx <marlxxja77@gmail.com>
2022-05-13 18:04:58 +0200

    Reimplemented cubemap

--- src/LynxEngine/Graphics/cubemap.cpp
+++ src/LynxEngine/Graphics/cubemap.cpp
@@ -3,6 +3,7 @@
 #include "cubemap.h"
 #include "texture.h"
 #include "Core/application.h"
+#include "rendererAPI.h"
 
 namespace Lynx::Graphics {
 
@@ -52,61 +53,6 @@ namespace Lynx::Graphics {
 	     1.0f, -1.0f,  1.0f
 	};
 
-	/*
-
-	CubemapTexture::CubemapTexture() : TextureBase(TEXTURE_CUBEMAP) 
-	{
-		glGenTextures(1, &texture);
-	    
-	}
-
-	void CubemapTexture::Load()
-	{
-		if(data->GetPath() == NULL)
-			log_warn("CubemapTexture::Load() : Texture data must include a path");
-		else
-			loadFromFile(data->GetPath());
-	}
-
-	void CubemapTexture::Generate() 
-	{
-		if(data == nullptr | data == nullptr | data->GetPath() == NULL) { log_error("CubemapTexture::Generate() : Invalid texture data"); return; }
-		
-		log_debug("Cubemap texture generate : %s", data->GetPath());
-		
-		switch ( IRendererAPI::GetAPI() ) {
-			case API_OPENGL:
-				glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-				glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-				glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-				glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-				glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-				glTexImage2D( GL_TEXTURE_CUBE_MAP_POSITIVE_X + count, 0, GL_RGB, data->GetWidth(), data->GetHeight(), 0, GL_RGB, GL_UNSIGNED_BYTE, data->GetData());
-				unsigned char* rawdata = data->GetData();
-				log_debug("Texture data : Path=%s W=%d H=%d Data length=%d", data->GetPath(), data->GetWidth(), data->GetHeight(), sizeof(rawdata) / sizeof(*rawdata));
-				glGenerateMipmap(GL_TEXTURE_2D);
-				break;
-		}
-		API_CheckErrors();
-		log_debug("After cubemap texture generate");
-		count++;
-		data->Free();
-		if(count==6) {
-			id = TextureBase::PushTextureID();
-			log_debug("New ID for cubemap texture : %d, texture %d", id, texture);
-		}
-	}
-
-	void CubemapTexture::Use()
-	{
-		switch ( IRendererAPI::GetAPI() ) {
-			case API_OPENGL:
-				glActiveTexture(GL_TEXTURE0 + id);
-				glBindTexture(GL_TEXTURE_CUBE_MAP, texture);
-				break;
-		}
-	}
-
 	Cubemap::Cubemap()
 	{
 	}
@@ -128,19 +74,21 @@ namespace Lynx::Graphics {
 		//glGenTextures(1, &texture->texture);
 		//glBindTexture(GL_TEXTURE_CUBE_MAP, texture->texture);
 
-        VAO = new VertexArray();
-		glGenBuffers(1, &VBO);
+        VAO = VertexArray::Create();
+		VBO = VertexBuffer::Create();
 		VAO->Bind();
-		glBindBuffer(GL_ARRAY_BUFFER, VBO);  
+		VBO->SetData(&cubemapVertices, sizeof(cubemapVertices));
+		VBO->Configure(MESH_3D);
+		/*glBindBuffer(GL_ARRAY_BUFFER, VBO);  
 		glBufferData(GL_ARRAY_BUFFER, sizeof(cubemapVertices), &cubemapVertices, GL_STATIC_DRAW);
 		glEnableVertexAttribArray(0);
-	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);*/
 
 	}
 
 	Cubemap::~Cubemap()
 	{
-		delete VAO, shader;
+
 	}
 
 	void Cubemap::Use(glm::mat4 projection, glm::mat4 view)
@@ -156,6 +104,6 @@ namespace Lynx::Graphics {
 		glDrawArrays(GL_TRIANGLES, 0, 36);
 		glBindVertexArray(0);
 		glDepthFunc(GL_LESS);
-	}*/
+	}
 
 }
--- src/LynxEngine/Graphics/cubemap.h
+++ src/LynxEngine/Graphics/cubemap.h
@@ -11,31 +11,18 @@
 
 namespace Lynx::Graphics {
 
-	/*
-	class LYNXENGINE_API CubemapTexture : public TextureBase {
-		public:
-			CubemapTexture();
-			void Load();
-			void Load(const char* path) { LoadFromFile(path); }
-			void Generate();
-			void Use();
-			friend class Cubemap;
-		private:
-			int count = 0;
-	};*/
-
 	class LYNXENGINE_API Cubemap{
 		public:
 			Cubemap();
 			~Cubemap();
-			void Load(std::vector<const char*>* textures) {}
-			void Use(glm::mat4 projection, glm::mat4 view) {}
+			void Load(std::vector<const char*>* textures);
+			void Use(glm::mat4 projection, glm::mat4 view); 
 			
 		private:
 			std::shared_ptr<TextureBase> texture;
 			std::shared_ptr<Shader> shader;
-			VertexArray* VAO;
-			GLuint VBO;
+			std::unique_ptr<VertexArray> VAO;
+			std::unique_ptr<VertexBuffer> VBO;
 	};
 
 }
